9994933a2f1344746276f35032d3d9ba617256ff
Dorian Neto <doriansampaioneto@gmail.com>
2020-06-07 13:58:39 -0700

    fix api url used in interface

--- public/js/app.js
+++ public/js/app.js
@@ -52083,7 +52083,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
 
 var api = axios__WEBPACK_IMPORTED_MODULE_0___default.a.create({
-  baseURL: 'http://localhost:8000/api/game/'
+  baseURL: '/api/game/'
 });
 /* harmony default export */ __webpack_exports__["default"] = (api);
 
--- resources/js/services/api.js
+++ resources/js/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/game/',
+  baseURL: '/api/game/',
 });
 
 export default api;
